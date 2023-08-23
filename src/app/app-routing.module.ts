5dfb6f770964f46b1b528cecdf4d02b4c06b6297
何志坚 <hezj@eastcom-sw.com>
2023-08-23 17:37:03 +0800

    界面整体布局和路由配置

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'canvas',
+    loadChildren: () => import('./modules/canvas/canvas.module').then(m => m.CanvasModule)
+  },
+  {
+    path: 'three',
+    loadChildren: () => import('./modules/three/three.module').then(m => m.ThreeModule)
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
